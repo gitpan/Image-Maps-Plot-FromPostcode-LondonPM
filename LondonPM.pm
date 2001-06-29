82409195324c86739b130d5ae7617c11c74e0894
Lee ♫ Goddard <lgoddard@cpan.org>
2001-06-29 20:21:23 +0000

    Import of LGODDARD/Image-Maps-Plot-FromPostcode-LondonPM-0.01 from CPAN.
    gitpan-cpan-distribution: Image-Maps-Plot-FromPostcode-LondonPM
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         LGODDARD/Image-Maps-Plot-FromPostcode-LondonPM-0.01.tar.gz
    gitpan-cpan-author:       LGODDARD
    gitpan-cpan-maturity:     released

---
+++ LondonPM.pm
@@ -0,0 +1,33 @@
+package Image::Maps::Plot::FromPostcode::LondonPM;
+use Image::Maps::Plot::FromPostcode;
+
+our $VERSION = 0.01;
+
+$Image::Maps::Plot::FromPostcode::chat = 1;
+
+# Adjust this to add a user
+# Image::Maps::Plot::FromPostcode::load_db (".london.pm.dat");
+# Image::Maps::Plot::FromPostcode::add_entry ('Peter Haworth at home','United Kingdom','BS7 9JT');
+# Image::Maps::Plot::FromPostcode::save_db (".london.pm.dat");
+
+# Create all maps
+# &Image::Maps::Plot::FromPostcode::all("E:/src/pl/");
+
+# Create one map
+# new Image::Maps::Plot::FromPostcode (MAP=>"LONDON","PATH"=>"E:/src/pl/out.html");
+
+exit;
+
+__END__;
+
+=head1 NAME
+
+Image::Maps::Plot::FromPostcode::LondonPM - do the London.pm maps
+
+=head1 DESCRIPTION
+
+Example of using Image::Maps::Plot::FromPostcode. Please see the source.
+
+=head1 AUTHOR
+
+Lee <LGoddard@CPAN.org>
